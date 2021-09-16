c5309f7420714038952cde2865d5d2a07a06fa3e
MiguelMorenoGit <katsura501@gmail.com>
2021-09-16 15:08:43 +0200

    navbar looks almost nice

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faTwitter , faLinkedinIn, faInstagram} from '@fortawesome/free-brands-svg-icons';
 
 export interface INavbarProps {
 }
@@ -7,8 +9,54 @@ export function Navbar (props: INavbarProps) {
   return (
 
     <>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container-fluid">
+        <header className="cs-header">
+            <div className="cs-header__wrapp">
+                <div className="cs-header__content-wr">
+                    <div className="cs-logo cs-logo--header">
+                        <a className="cs-logo__link" href="">
+                            <img src={'logo-black.png'} alt="" className="image_logo"/>
+                        </a>
+                        <a className="cs-logo__link" href="" >
+                            <img src={'logo-white.png'} alt="" className="image_logo" style={{backgroundColor: 'black'}}/>
+                        </a>
+                    </div>
+                    <div className="cs-header__nav">
+                        <div className="cs-header__inner">
+                            <nav>
+                                <ul className="main-menu">
+                                    <li className="menu-item current-menu-ancestor"><a href="">HOME</a></li>
+                                    <li className="menu-item current-menu-ancestor"><a href="">SERVICIOS</a></li>
+                                    <li className="menu-item current-menu-ancestor"><a href="">PROJECTOS</a></li>
+                                    <li className="menu-item current-menu-ancestor"><a href="">CONTACTO</a></li>
+                                    <li className="menu-item current-menu-ancestor"><a href="">BLOG</a></li>
+                                </ul>
+                            </nav>
+
+                        </div>
+                    </div>
+                    <div className="cs-header__nav-btn">
+                        <ul className="cs-socials cs-socials--menu">
+                            <li className="cs-socials__item">
+                                <a target="_blank" href="#"><FontAwesomeIcon icon={faFacebookF} className="fa"/></a>
+                                
+                            </li>
+                            <li className="cs-socials__item">
+                                <a target="_blank" href="#"><FontAwesomeIcon icon={faTwitter} className="fa"/></a>
+                                
+                            </li>
+                            <li className="cs-socials__item">
+                                <a target="_blank" href="#"><FontAwesomeIcon icon={faLinkedinIn} className="fa"/></a>
+                                
+                            </li>
+                        </ul>
+
+                    </div>
+
+
+                </div>
+
+
+                {/* </div>
                 <a href="/" className="navbar-brand">DSEA servicio de Arquitectura</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -24,12 +72,12 @@ export function Navbar (props: INavbarProps) {
                 
                 
                 <div className="col-md-3 text-end">
-                <i>Icon Facebook</i>
-                <i>Icon Linkedin</i>
-                </div>
+                    <i>Icon Facebook</i>
+                    <i>Icon Linkedin</i>
+                </div> */}
             </div>            
 
-        </nav>
+        </header>
         
     </>
     
